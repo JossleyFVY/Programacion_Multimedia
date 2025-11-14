09591bc2e5e1c1d06afed2ae233cd743969af0c8
jvy0001@alu.medac.es <jvy0001@alu.medac.es>
2025-11-14 17:30:23 +0100

    tema7_2_Finalizado
    añadidos los soundpool

--- Tarea_tema_7_2/app/src/main/java/com/example/tarea_tema_7/MainActivity.java
+++ Tarea_tema_7_2/app/src/main/java/com/example/tarea_tema_7/MainActivity.java
@@ -12,6 +12,7 @@ import android.os.Handler;
 import android.provider.MediaStore;
 import android.widget.*;
 import android.media.MediaPlayer;
+import android.media.SoundPool;
 
 import java.io.IOException;
 
@@ -22,6 +23,11 @@ public class MainActivity extends AppCompatActivity {
     private Button btnPlay;
     private Button btnPausa;
     private SeekBar reproduccionBarra;
+    private Button btnSonido1;
+    private Button btnSonido2;
+    private Button btnSonido3;
+    private Button btnSonido4;
+
     private String formatTime(int ms){ // para convertir el tiempo de milisegundos
         int segundos= (ms / 1000) % 60;
         int minutos = (ms/1000) /60;
@@ -48,8 +54,28 @@ public class MainActivity extends AppCompatActivity {
         btnPausa = findViewById(R.id.btnPausa);
         reproduccionBarra = findViewById(R.id.reproduccionBarra);
         mediaPlayer = MediaPlayer.create(this,R.raw.golden);
+        btnSonido1 = findViewById(R.id.btnSonido1);
+        btnSonido2 = findViewById(R.id.btnSonido2);
+        btnSonido3 = findViewById(R.id.btnSonido3);
+        btnSonido4 = findViewById(R.id.btnSonido4);
 
-
+        SoundPool soundPool = new SoundPool.Builder().setMaxStreams(10).build();
+        int sonido1= soundPool.load(this,R.raw.sonido1,1);
+        int sonido2= soundPool.load(this,R.raw.sonido2,1);
+        int sonido3= soundPool.load(this,R.raw.sonido3,1);
+        int sonido4= soundPool.load(this,R.raw.sonido4,1);
+        btnSonido1.setOnClickListener(v -> {
+            soundPool.play(sonido1,1,1,1,0,1);
+        });
+        btnSonido2.setOnClickListener(v -> {
+            soundPool.play(sonido2,1,1,1,0,1);
+        });
+        btnSonido3.setOnClickListener(v -> {
+            soundPool.play(sonido3,1,1,1,0,1);
+        });
+        btnSonido4.setOnClickListener(v -> {
+            soundPool.play(sonido4,1,1,1,0,1);
+        });
         btnPlay.setOnClickListener(v ->{
             mediaPlayer.start();
         });
